9e3678054bd3ccc66f153d4b6044a3f92f5c69a3
madhusudhanpallela05 <151707585+madhusudhanpallela05@users.noreply.github.com>
2025-11-28 09:33:48 +0530

    Update distancevector.c

--- distancevector.c
+++ distancevector.c
@@ -1,91 +1,41 @@
+DISTANCE VECTOR 
 #include <stdio.h>
-#define INF 999
-#define MAX 20
-
-struct RoutingTable
-{
-    int dist[MAX];
-    int nextHop[MAX];
-};
-
-void main()
-{
-    int n;
-    int cost[MAX][MAX];
-    char nodes[MAX][20];
-    struct RoutingTable rt[MAX];
-
-    printf("Enter number of nodes (hosts/routers): ");
+ #define INF 999
+ int main() {
+    int n, cost[10][10], dist[10][10];
+    printf("Enter number of nodes: ");
     scanf("%d", &n);
-
-    printf("Enter node names (e.g., H1, H2):\n");
+    printf("Enter cost matrix:\n");
     for (int i = 0; i < n; i++)
-    {
-        printf("Node %d: ", i + 1);
-        scanf("%19s", nodes[i]);
-    }
-
-    printf("\nEnter adjacency matrix (delay cost, INF=999, 0 for self):\n");
-    for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
             scanf("%d", &cost[i][j]);
-    }
-
-    // Initialize routing tables
     for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
-        {
-            rt[i].dist[j] = cost[i][j];
-            if (cost[i][j] != INF && i != j)
-                rt[i].nextHop[j] = j;
-            else
-                rt[i].nextHop[j] = -1;
-        }
-    }
-
-    int updated;
-    do
-    {
-        updated = 0;
+            dist[i][j] = cost[i][j];
+    for (int k = 0; k < n; k++)
         for (int i = 0; i < n; i++)
-        {
             for (int j = 0; j < n; j++)
-            {
-                for (int k = 0; k < n; k++)
-                {
-                    // minimal safety guard for INF values
-                    if (cost[i][k] < INF && rt[k].dist[j] < INF &&
-                        rt[i].dist[j] > cost[i][k] + rt[k].dist[j])
-                    {
-                        rt[i].dist[j] = cost[i][k] + rt[k].dist[j];
-                        rt[i].nextHop[j] = k;
-                        updated = 1;
-                    }
-                }
-            }
-        }
-    } while (updated);
-
-    // Print Routing Tables
-    for (int i = 0; i < n; i++)
-    {
-        printf("\nRouting Table for Node %s\n", nodes[i]);
-        printf("Destination\tNextHop\t\tCost\n");
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+    printf("\nFinal Distance Table:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Node %d: ", i);
         for (int j = 0; j < n; j++)
-        {
-            if (i == j) continue;
-
-            printf("%s\t\t", nodes[j]);
-            if (rt[i].dist[j] == INF || rt[i].nextHop[j] == -1)
-            {
-                printf("-\t\tINF\n");
-            }
-            else
-            {
-                printf("%s\t\t%d\n", nodes[rt[i].nextHop[j]], rt[i].dist[j]);
-            }
-        }
+            printf("%d ", dist[i][j]);
+        printf("\n");
     }
-}
+    return 0;
+ }
+OUTPUT:
+Enter number of nodes: 4
+Enter cost matrix:
+0 5 1 999
+5 0 3 2
+1 3 0 2
+999 2 2 0
+
+Final Distance Table:
+Node 0: 0 4 1 3 
+Node 1: 4 0 3 2 
+Node 2: 1 3 0 2 
+Node 3: 3 2 2 0
